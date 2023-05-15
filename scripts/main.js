448224dfd962f92ebdb403c35327ced1548a6e4d
Pablo Noviello <pablonoviello@outlook.com>
2023-05-15 20:57:57 +0200

    secciones y botones

--- scripts/main.js
+++ scripts/main.js
@@ -54,12 +54,103 @@ let hideModal = function () {
 }
 
 let modalContent = `<div class="modal-dialog">
-<div class="modal-content">
-  <div class="modal-header">
-    <h5 class="modal-title">Ayuda</h5>
+  <div class="modal-content d-flex flex-auto flex-column">
+    <div class="modal-header justify-content-center">
+      <h1 class="modal-title">Keyboard shortcuts</h1>
+    </div>
+    <div class="modal-body d-flex justify-content-center">
+      <ul class="col-9 list-inline justify-content-center align-items-center">
+        <li class="d-flex justify-content-between align-items-center">
+          <div>Bring up this help dialog</div>
+          <div>
+            <kbd>
+              <span>
+                ?
+              </span>
+            </kbd>
+          </div>
+        </li>
+        <li class="d-flex justify-content-between">
+          <div class="flex-auto">Go to "Who We are?" section</div>
+          <div class="ml-2 no-wrap">
+            <kbd>
+              <span>
+                1
+              </span>
+            </kbd>
+          </div>
+        </li>
+        <li class="d-flex justify-content-between">
+          <div class="flex-auto">Go to "Services" section</div>
+          <div class="ml-2 no-wrap">
+            <kbd>
+              <span>
+                2
+              </span>
+            </kbd>
+          </div>
+        </li>
+        <li class="d-flex justify-content-between">
+          <div class="flex-auto">Go to "Portfolio" section</div>
+          <div class="ml-2 no-wrap">
+            <kbd>
+              <span>
+                3
+              </span>
+            </kbd>
+          </div>
+        </li>
+        <li class="d-flex justify-content-between">
+          <div class="flex-auto">Go to "Location" section</div>
+          <div class="ml-2 no-wrap">
+            <kbd>
+              <span>
+                4
+              </span>
+            </kbd>
+          </div>
+        </li>
+        <li class="d-flex justify-content-between">
+          <div class="flex-auto">Go to "Email Us" section</div>
+          <div class="ml-2 no-wrap">
+            <kbd>
+              <span>
+                5
+              </span>
+            </kbd>
+          </div>
+        </li>
+        <li class="d-flex justify-content-between">
+          <div class="flex-auto">Move to Next section</div>
+          <div class="ml-2 no-wrap">
+            <kbd>
+              <span>
+                n
+              </span>
+            </kbd>
+          </div>
+        </li>
+        <li class="d-flex justify-content-between">
+          <div class="flex-auto">Move to Previous section</div>
+          <div class="ml-2 no-wrap">
+            <kbd>
+              <span>
+                p
+              </span>
+            </kbd>
+          </div>
+        </li>
+        <li class="d-flex justify-content-between">
+          <div class="flex-auto">Close this help dialog</div>
+          <div class="ml-2 no-wrap">
+            <kbd>
+              <span>
+                Esc
+              </span>
+            </kbd>
+          </div>
+        </li>
+      </ul>
+    </div>
   </div>
-  <div class="modal-body">
-    <p>Modal body text goes here.</p>
-  </div>
-</div>
 </div>`
