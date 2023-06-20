25335352109b7d6568cab370ed50198313b45cb5
DESKTOP-E4IAULG\User <marc.molina.torres@gmail.com>
2023-06-20 21:16:29 +0200

    nav y footer base

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -6,35 +6,59 @@
 
   <main>
     <section id="frameListaTareasId" class="frameListaTareas">
-      <header id="menu"></header>
+      <header>
+        <nav id="menu">menu</nav>
+      </header>
+      <footer id="footer"></footer>
     </section>
   </main>
 
-  <footer>
 
-  </footer>
 </template>
 
 <style scoped>
 
-  .frameListaTareas {
-    width: 1520px;
-    height: 730px;
+  html, body, #app {
+  height: 100%;
+  margin: 0;
+}
+.frameListaTareas {
+    width: 98vw;
+    height: 96vh;
     background-color: #eeeded;
-    margin-bottom: 10px;
+    margin-bottom: 2%;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    justify-content: flex-start;
     align-items: center;
-    border-radius: 10px; 
+    border-radius: 1%; 
   }
 
-  #menu{
-    background-color: #aaaaaa;
-    width: 1520px;
-    height: 20px;
-    margin-bottom: 10px;
-    align-items: flex-start;
-    display: flex;
+  #menu {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    align-items: center;
+    background: rgb(147, 198, 243);
+    font-family: sans-serif;
+    width: 98vw;
+    height: 6vh;
+    position: sticky;
+    top: 0;
+    text-align: center;
   }
+
+  #footer {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    align-items: center;
+    background: rgb(147, 198, 243);
+    font-family: sans-serif;
+    width: 98vw;
+    height: 6vh;
+    position: sticky;
+    top: 98vh;
+    text-align: center;
+  }
+  
 
 </style>
