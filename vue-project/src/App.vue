c9fd8e790a38e7941d93bd10d44dd8e7c563f3a4
DESKTOP-E4IAULG\User <marc.molina.torres@gmail.com>
2023-07-08 14:09:31 +0200

    detalles visuales del frameListaTareas corregidos

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -4,16 +4,18 @@
       <header>
         <Menu @agregar-tarea="agregarTarea" />
       </header>
-      <div id="tareas" class="tareas-container">
-        <div class="tarea" v-for="tarea in tareas" :key="tarea.id">
-          <Tarea
-            :nombre="tarea.nombre"
-            :completada="tarea.completada"
-            :descripcion="tarea.descripcion"
-            @borrar-tarea="borrarTarea(tarea.id)"
-            @update:descripcion="actualizarDescripcion(tarea.id, $event)"
-            @completar-tarea="completarTarea(tarea.id)"
-          />
+      <div class="tareas-content">
+        <div id="tareas" class="tareas-container">
+          <div class="tarea" v-for="tarea in tareas" :key="tarea.id">
+            <Tarea
+              :nombre="tarea.nombre"
+              :completada="tarea.completada"
+              :descripcion="tarea.descripcion"
+              @borrar-tarea="borrarTarea(tarea.id)"
+              @update:descripcion="actualizarDescripcion(tarea.id, $event)"
+              @completar-tarea="completarTarea(tarea.id)"
+            />
+          </div>
         </div>
       </div>
       <Footer />
@@ -68,6 +70,7 @@ html, body, #app {
   justify-content: flex-start;
   align-items: center;
   border-radius: 1%;
+  overflow: hidden;
 }
 
 .tareas-container {
@@ -79,7 +82,34 @@ html, body, #app {
   width: 100%;
 }
 
+.tareas-content {
+  flex-grow: 1;
+  overflow-y: auto;
+  overflow-x: hidden;
+  border-radius: 1%;
+  width: 100%;
+}
+
+.tareas-content::-webkit-scrollbar {
+  width: 10px;
+  background-color: #f1f1f1;
+}
+
+.tareas-content::-webkit-scrollbar-track {
+  background-color: #f1f1f1;
+}
+
+.tareas-content::-webkit-scrollbar-thumb {
+  background-color: #888;
+  border-radius: 5px;
+}
+
+.tareas-content::-webkit-scrollbar-thumb:hover {
+  background-color: #555;
+}
+
 .tarea {
   flex: 0 0 15em;
+  margin-right: 0.5em;
 }
 </style>
